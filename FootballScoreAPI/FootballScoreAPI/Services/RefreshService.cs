3db1e47d00a9b19b840472052c6a290cf8c8e687
John Watson <johnwatson484@hotmail.com>
2019-09-14 20:48:42 +0100

    Refresh refactored

--- FootballScoreAPI/FootballScoreAPI/Services/RefreshService.cs
+++ FootballScoreAPI/FootballScoreAPI/Services/RefreshService.cs
@@ -18,20 +18,24 @@ namespace FootballScoreAPI.Services
 
         public void Refresh()
         {
-            context.Database.ExecuteSqlCommand("DELETE FROM dbo.Fixtures");
-            context.Database.ExecuteSqlCommand("DBCC CHECKIDENT('dbo.Fixtures', RESEED, 0)");
-            context.Database.ExecuteSqlCommand("DBCC CHECKIDENT('dbo.Goals', RESEED, 0)");
+            ReseedDatabase();
 
             DateTime endDate = DateTime.Now.Date;
             DateTime startDate = endDate.AddDays(-10);
 
             for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
             {
-                var fixtures = scrapingService.ScrapeScores(date);
-                context.Fixtures.AddRange(fixtures);
+                context.Fixtures.AddRange(scrapingService.ScrapeScores(date));
             }
 
             context.SaveChanges();
+        }
+
+        private void ReseedDatabase()
+        {
+            context.Database.ExecuteSqlCommand("DELETE FROM dbo.Fixtures");
+            context.Database.ExecuteSqlCommand("DBCC CHECKIDENT('dbo.Fixtures', RESEED, 0)");
+            context.Database.ExecuteSqlCommand("DBCC CHECKIDENT('dbo.Goals', RESEED, 0)");
         }
 
         public void RefreshDay()
@@ -39,10 +43,7 @@ namespace FootballScoreAPI.Services
             DateTime date = DateTime.Now.Date;
 
             context.Fixtures.RemoveRange(context.Fixtures.Where(x => x.Date == date));
-
-            var fixture = scrapingService.ScrapeScores(date);
-            context.Fixtures.AddRange(fixture);
-
+            context.Fixtures.AddRange(scrapingService.ScrapeScores(date));
             context.SaveChanges();
         }
     }
